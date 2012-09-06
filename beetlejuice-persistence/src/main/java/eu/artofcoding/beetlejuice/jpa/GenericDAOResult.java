8599b4a069d74d028d745076b2e02d7b9ff5b297
Ralf Bensmann <ralf@art-of-coding.eu>
2012-09-06 16:20:07 +0200

    Added enum to designate result

---
+++ beetlejuice-persistence/src/main/java/eu/artofcoding/beetlejuice/jpa/GenericDAOResult.java
@@ -0,0 +1,16 @@
+/*
+ * beetlejuice
+ * beetlejuice-persistence
+ * Copyright (C) 2011-2012 art of coding UG, http://www.art-of-coding.eu/
+ *
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * rbe, 04.09.12 20:59
+ */
+
+package eu.artofcoding.beetlejuice.jpa;
+
+public enum GenericDAOResult {
+    OK, ERROR;
+}
