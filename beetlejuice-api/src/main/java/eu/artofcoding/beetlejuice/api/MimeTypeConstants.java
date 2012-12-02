3e1a1ceaa1183431081a82738f1a6608124c48db
Ralf Bensmann <ralf@art-of-coding.eu>
2012-12-02 21:27:03 +0100

    Added constants for MimeTypes

---
+++ beetlejuice-api/src/main/java/eu/artofcoding/beetlejuice/api/MimeTypeConstants.java
@@ -0,0 +1,100 @@
+package eu.artofcoding.beetlejuice.api;
+
+public class MimeTypeConstants {
+
+    public static final String APPLICATON_OCTET_STREAM = "application/octet-stream".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT = "application/vnd.oasis.opendocument.text".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT_TEMPLATE = "application/vnd.oasis.opendocument.text-template".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT_WEB = "application/vnd.oasis.opendocument.text-web".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT_MASTER = "application/vnd.oasis.opendocument.text-master".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_GRAPHICS = "application/vnd.oasis.opendocument.graphics".intern();
+    public static final String APPLICATION_VND_MS_POWERPOINT_SLIDESHOW_MACRO_ENABLED_12 = "application/vnd.ms-powerpoint.slideshow.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_MS_POWERPOINT_TEMPLATE_MACRO_ENABLED_12 = "application/vnd.ms-powerpoint.template.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_MS_POWERPOINT_PRESENTATION_MACRO_ENABLED_12 = "application/vnd.ms-powerpoint.presentation.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_MS_POWERPOINT_ADDIN_MACRO_ENABLED_12 = "application/vnd.ms-powerpoint.addin.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_SLIDESHOW = "application/vnd.openxmlformats-officedocument.presentationml.slideshow".intern();
+    public static final String APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_TEMPLATE = "application/vnd.openxmlformats-officedocument.presentationml.template".intern();
+    public static final String APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_PRESENTATION = "application/vnd.openxmlformats-officedocument.presentationml.presentation".intern();
+    public static final String APPLICATION_VND_MS_EXCEL_SHEET_BINARY_MACRO_ENABLED_12 = "application/vnd.ms-excel.sheet.binary.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_MS_EXCEL_ADDIN_MACRO_ENABLED_12 = "application/vnd.ms-excel.addin.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_MS_EXCEL_TEMPLATE_MACRO_ENABLED_12 = "application/vnd.ms-excel.template.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_MS_EXCEL_SHEET_MACRO_ENABLED_12 = "application/vnd.ms-excel.sheet.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_TEMPLATE = "application/vnd.openxmlformats-officedocument.spreadsheetml.template".intern();
+    public static final String APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_SHEET = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet".intern();
+    public static final String APPLICATION_VND_MS_WORD_TEMPLATE_MACRO_ENABLED_12 = "application/vnd.ms-word.template.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_MS_WORD_DOCUMENT_MACRO_ENABLED_12 = "application/vnd.ms-word.document.macroEnabled.12".intern();
+    public static final String APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_TEMPLATE = "application/vnd.openxmlformats-officedocument.wordprocessingml.template".intern();
+    public static final String APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT = "application/vnd.openxmlformats-officedocument.wordprocessingml.document".intern();
+    public static final String APPLICATION_VND_MS_POWERPOINT = "application/vnd.ms-powerpoint".intern();
+    public static final String APPLICATION_VND_MS_EXCEL = "application/vnd.ms-excel".intern();
+    public static final String APPLICATION_MSWORD = "application/msword".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_IMAGE = "application/vnd.oasis.opendocument.image".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_FORMULA = "application/vnd.oasis.opendocument.formula".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_CHART = "application/vnd.oasis.opendocument.chart".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_SPREADSHEET_TEMPLATE = "application/vnd.oasis.opendocument.spreadsheet-template".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_SPREADSHEET = "application/vnd.oasis.opendocument.spreadsheet".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_PRESENTATION_TEMPLATE = "application/vnd.oasis.opendocument.presentation-template".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_PRESENTATION = "application/vnd.oasis.opendocument.presentation".intern();
+    public static final String APPLICATION_VND_OASIS_OPENDOCUMENT_GRAPHICS_TEMPLATE = "application/vnd.oasis.opendocument.graphics-template".intern();
+
+    public static final String TEXT_PLAIN = "text/plain".intern();
+    public static final String TEXT_HTML = "text/html".intern();
+
+    public static final String IMAGE_JPEG = "image/jpeg".intern();
+    public static final String IMAGE_TIF = "image/tif".intern();
+    public static final String IMAGE_TIFF = IMAGE_TIF;
+    public static final String IMAGE_PNG = "image/png".intern();
+    public static final String APPLICATION_PDF = "application/pdf".intern();
+    public static final String APPLICATION_RTF = "application/rtf".intern();
+
+    public static final String S_TIFF = "tiff".intern();
+    public static final String S_TIF = "tif".intern();
+    public static final String S_JPEG = "jpeg".intern();
+    public static final String S_JPG = "jpg".intern();
+    public static final String S_PNG = "png".intern();
+    public static final String S_PDF = "pdf".intern();
+    public static final String S_RTF = "rtf".intern();
+
+    public static final String S_ODT = "odt".intern();
+    public static final String S_OTT = "ott".intern();
+    public static final String S_OTH = "oth".intern();
+    public static final String S_ODM = "odm".intern();
+    public static final String S_ODG = "odg".intern();
+    public static final String S_OTG = "otg".intern();
+    public static final String S_ODP = "odp".intern();
+    public static final String S_OTP = "otp".intern();
+    public static final String S_ODS = "ods".intern();
+    public static final String S_OTS = "ots".intern();
+    public static final String S_ODC = "odc".intern();
+    public static final String S_ODF = "odf".intern();
+    public static final String S_ODI = "odi".intern();
+    public static final String S_DOC = "doc".intern();
+    public static final String S_DOT = "dot".intern();
+    public static final String S_XLS = "xls".intern();
+    public static final String S_XLT = "xlt".intern();
+    public static final String S_XLA = "xla".intern();
+    public static final String S_PPT = "ppt".intern();
+    public static final String S_POT = "pot".intern();
+    public static final String S_PPS = "pps".intern();
+    public static final String S_PPA = "ppa".intern();
+    public static final String S_DOCX = "docx".intern();
+    public static final String S_DOTX = "dotx".intern();
+    public static final String S_DOCM = "docm".intern();
+    public static final String S_DOTM = "dotm".intern();
+    public static final String S_XLSX = "xlsx".intern();
+    public static final String S_XLTX = "xltx".intern();
+    public static final String S_XLSM = "xlsm".intern();
+    public static final String S_XLTM = "xltm".intern();
+    public static final String S_XLAM = "xlam".intern();
+    public static final String S_XLSB = "xlsb".intern();
+    public static final String S_PPTX = "pptx".intern();
+    public static final String S_POTX = "potx".intern();
+    public static final String S_PPSX = "ppsx".intern();
+    public static final String S_PPAM = "ppam".intern();
+    public static final String S_PPTM = "pptm".intern();
+    public static final String S_POTM = "potm".intern();
+    public static final String S_PPSM = "ppsm".intern();
+
+    public static final String S_UTF8 = "UTF-8".intern();
+
+}
