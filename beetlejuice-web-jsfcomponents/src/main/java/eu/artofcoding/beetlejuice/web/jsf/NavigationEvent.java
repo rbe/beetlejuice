dc0ae969705949163115c44a6e30e215d9dff60c
Ralf Bensmann <ralf@art-of-coding.eu>
2013-02-18 19:44:59 +0100

    Added serialVersionUID

--- beetlejuice-web-jsfcomponents/src/main/java/eu/artofcoding/beetlejuice/web/jsf/NavigationEvent.java
+++ beetlejuice-web-jsfcomponents/src/main/java/eu/artofcoding/beetlejuice/web/jsf/NavigationEvent.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class NavigationEvent implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private final String viewId;
 
     public NavigationEvent(String viewId) {
