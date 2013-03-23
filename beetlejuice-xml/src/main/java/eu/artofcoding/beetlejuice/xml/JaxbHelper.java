d284e3fd13d0971361b0993900b8256621d32ad9
Ralf Bensmann <ralf@art-of-coding.eu>
2013-03-23 15:05:50 +0100

    Updated header

--- beetlejuice-xml/src/main/java/eu/artofcoding/beetlejuice/xml/JaxbHelper.java
+++ beetlejuice-xml/src/main/java/eu/artofcoding/beetlejuice/xml/JaxbHelper.java
@@ -1,12 +1,12 @@
 /*
  * beetlejuice
  * beetlejuice-xml
- * Copyright (C) 2011-2013 art of coding UG, http://www.art-of-coding.eu/
+ * Copyright (C) 2011-2013 art of coding UG, http://www.art-of-coding.eu
  *
  * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
  * All rights reserved. Use is subject to license terms.
  *
- * rbe, 13.01.13 12:54
+ * rbe, 18.02.13 19:49
  */
 
 package eu.artofcoding.beetlejuice.xml;
--- beetlejuice-xml/src/main/java/eu/artofcoding/beetlejuice/xml/XmlHelper.java
+++ beetlejuice-xml/src/main/java/eu/artofcoding/beetlejuice/xml/XmlHelper.java
@@ -1,3 +1,14 @@
+/*
+ * beetlejuice
+ * beetlejuice-xml
+ * Copyright (C) 2011-2013 art of coding UG, http://www.art-of-coding.eu
+ *
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * rbe, 18.02.13 19:49
+ */
+
 package eu.artofcoding.beetlejuice.xml;
 
 import org.w3c.dom.Document;
