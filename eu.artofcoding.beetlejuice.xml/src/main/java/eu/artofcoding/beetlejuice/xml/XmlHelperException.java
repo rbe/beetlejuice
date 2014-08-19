78ef34a48e2a3b3db66202d9b94b396d13869a24
Ralf Bensmann <ralf@art-of-coding.eu>
2014-08-19 08:15:21 +0200

    Refactored Maven modules to match artifactId

---
+++ eu.artofcoding.beetlejuice.xml/src/main/java/eu/artofcoding/beetlejuice/xml/XmlHelperException.java
@@ -0,0 +1,28 @@
+/*
+ * beetlejuice
+ * beetlejuice-xml
+ * Copyright (C) 2011-2013 art of coding UG, http://www.art-of-coding.eu
+ *
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * rbe, 18.02.13 19:49
+ */
+
+package eu.artofcoding.beetlejuice.xml;
+
+public class XmlHelperException extends Exception {
+
+    public XmlHelperException(Throwable cause) {
+        super(cause);
+    }
+
+    public XmlHelperException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public XmlHelperException(String message) {
+        super(message);
+    }
+
+}
